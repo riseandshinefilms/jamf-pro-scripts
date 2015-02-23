0832d90e6b6a1838c07bbb48d6d8ccdc64853136
Will Pierce <core.editor>
2015-02-23 11:55:17 -0600

    updatesCheckDisplay.sh removed old Apple update code

--- updatesCheckDisplay.sh
+++ updatesCheckDisplay.sh
@@ -4,10 +4,13 @@
 #### 3rd party updates must be cached via JAMFs JSS
 #### Will Pierce
 #### 140804
+#### Updated February 23, 2015
+
 #### Requirements
 #### http://mstratman.github.io/cocoadialog/
 CD_APP="/Applications/Utilities/CocoaDialog.app"
 CD="$CD_APP/Contents/MacOS/CocoaDialog"
+
 ######--------------------------------------------
 # create a named pipe
 rm -f /tmp/hpipe
@@ -65,30 +68,6 @@ if [[ '$UpdatesRunDate' != "" ]]; then
 else
    UpdatesRunDate="N/A"
 fi
-###------ -------------
-##### OLD APPPLE UPDATES 86 if other command works.
-## Create a variable for Apple update command. 86 the lines we dont need.
-# This way shows just the update name ------
-# appleUpdates=`softwareupdate -l | sed 's/Software Update Tool//g;s/Copyright 2002-2012 Apple Inc.//g;s/Finding available software//g;s/Software Update found the following new or updated software://' | grep '*' | sed 's/*//'`
-# sed 's/*//' removes the *
-# grep '*' Displays only lines with the *
-## this way shows update with file size and recommend and restart
-# appleUpdates=`softwareupdate -l | sed 's/Software Update Tool//g;s/Copyright 2002-2012 Apple Inc.//g;s/Finding available software//g;s/Software Update found the following new or updated software://' | sed '/*/d' | grep '.'`
-
-
-
-# ## If there are no Apple updates then create new variable for noau set to No apple updates
-# if [ "$appleUpdates" == "No new software available." ];then
-# noau="No Apple Updates needed"
-# ## If there are Apple updates set variable noau to nothing
-# else
-# noau=""
-# fi
-## Check for JAMF waiting room, if so create variable for getting the results of waiting room command
-
-####----------------------------
-
-
 
 ########## Apple Updates
 
