1675d14bc30317e27be976e1beb59b55657d2190
Will Pierce <will.pierce@collemcvoy.com>
2015-05-11 15:39:43 -0500

    PhotosAppMoveToApplications_HiddenFolder.sh added a var for the hidden folder fixed some errors

--- PhotosAppMoveToApplications_HiddenFolder.sh
+++ PhotosAppMoveToApplications_HiddenFolder.sh
@@ -4,44 +4,51 @@
 # This will prevent users finding Photos in the Applications folder, it will also not show in a spotlight search.
 # Will Pierce
 # May 11, 2015
+# Set variables
+hiddenFolder="/Users/macroot/Library/Applications_Hidden/"
 
 # check for the the Applications_Hidden folder
-if [ ! -d "/Users/macroot/Library/Applications_Hidden/"]; then
-	echo "/Users/macroot/Library/Applications_Hidden/ NOT found, creating..."
+if [ ! -d "$hiddenFolder" ]; then
+	echo "$hiddenFolder NOT found, creating..."
 
 	# make the Applications_Hidden folder
-	mkdir /Users/macroot/Library/Applications_Hidden/
+	mkdir $hiddenFolder
 
 	# Test to see that it was created
-			if [ -d "/Users/macroot/Library/Applications_Hidden/"]; then
-				echo "/Users/macroot/Library/Applications_Hidden/ created."
+			if [ -d "$hiddenFolder" ]; then
+				echo "$hiddenFolder created."
 					else
 						echo "Users/macroot/Library/Applications_Hidden/ NOT created something went wrong."
 			fi
 	else
-		echo "/Users/macroot/Library/Applications_Hidden/ found."
+		echo "$hiddenFolder folder found."
 fi
 
 # Check to see if Photos.app is installed 
-if [-e /Applications/Photos.app ]; then
+if [ -e /Applications/Photos.app ]; then
 	echo "Photos.app found moving to /Users/macroot/Library/Applications_Hidden"
 
 	# Move the Photos app to Applications_Hidden
-	mv /Applications/Photos.app /Users/macroot/Library/Applications_Hidden/
+	mv /Applications/Photos.app $hiddenFolder
 
 	# Test that it was moved
 	# Make sure Photos.app is gone			Make sure Photos.app is now in /Users/macroot/Library/Applications_Hidden
-	if [ ! -e /Applications/Photos.app ] && [ -e /Users/macroot/Library/Applications_Hidden/Phots.app ]; then
+	if [ ! -e /Applications/Photos.app ] && [ -e $hiddenFolder/Photos.app ]; then
 		echo "Photos.app moved from Applications folder to macroot/Library/Applications_Hidden successfully"
+		echo "Setting Spotlight to re index"
+			# erase the Spotlight index
+ 			mdutil -E /
+
+ 			# turn Spotlight indexing off
+ 			mdutil -i off /
+
+ 			# turn Spotlight indexing back on
+ 			mdutil -i on /
 		else
-			echo "something went wrong Photos.app not in Applications and not in /Users/macroot/Library/Applications_Hiddens"
+			echo "something went wrong Photos.app not in Applications and not in /Users/macroot/Library/Applications_Hidden"
 	fi
 else
 	echo "Photos.app NOT found, nothing to move."
 fi 
-
-## Reminder: when moving applications they might still show in spotlight.
-## Run the Spotlight erase and rebuild index script 
-### SpotlightEraseAndRebuildIndexes.sh
 
 exit 0
